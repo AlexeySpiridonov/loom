ccbed12288548b350378655e921ad8c379acdcec
AlteredGame <ralegond@gmail.com>
2021-10-17 14:46:31 +0300

    perfectly

--- lib/screens/successful_screen.dart
+++ lib/screens/successful_screen.dart
@@ -26,44 +26,75 @@ class SuccessfulScreen extends StatelessWidget {
       height: 1.4,
     );
     final textStyle2 = TextStyle(
-      fontSize: 18 * devicePixelRatio,
-      fontWeight: FontWeight.w600,
+      fontSize: 14 * devicePixelRatio,
+      fontWeight: FontWeight.w400,
+      color: const Color.fromRGBO(255, 255, 255, 0.4),
       height: 1.4,
     );
     final textStyle3 = TextStyle(
-      fontSize: 18 * devicePixelRatio,
-      fontWeight: FontWeight.w600,
+      fontSize: 16 * devicePixelRatio,
+      fontWeight: FontWeight.w400,
       height: 1.4,
     );
 
     return Scaffold(
-      body: LoomBody(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-              top: 52.0 * devicePixelRatio,
-              bottom: 37.0 * devicePixelRatio,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 52.0 * devicePixelRatio,
+                  bottom: 37.0 * devicePixelRatio,
+                ),
+                child: Column(children: [
+                  Text(
+                    AppLocalizations.of(context)!.message9_1,
+                    textAlign: TextAlign.start,
+                    style: textStyle1,
+                  ),
+                  SizedBox(height: 25.0 * devicePixelRatio),
+                  Text(
+                    AppLocalizations.of(context)!.message9_2,
+                    textAlign: TextAlign.start,
+                    style: textStyle2,
+                  ),
+                  SizedBox(height: 2.0 * devicePixelRatio),
+                  Text(
+                    networkName,
+                    textAlign: TextAlign.start,
+                    style: textStyle3,
+                  ),
+                  SizedBox(height: 25.0 * devicePixelRatio),
+                  Text(
+                    AppLocalizations.of(context)!.message9_3,
+                    textAlign: TextAlign.start,
+                    style: textStyle2,
+                  ),
+                  SizedBox(height: 2.0 * devicePixelRatio),
+                  Text(
+                    loomName,
+                    textAlign: TextAlign.start,
+                    style: textStyle3,
+                  ),
+                  SizedBox(height: 25.0 * devicePixelRatio),
+                  Text(
+                    AppLocalizations.of(context)!.message9_4,
+                    textAlign: TextAlign.start,
+                    style: textStyle3,
+                  ),
+                ]),
+              ),
             ),
-            child: Column(children: [
-              Text(AppLocalizations.of(context)!.message9_1,
-                  textAlign: TextAlign.start, style: textStyle1),
-              Text(AppLocalizations.of(context)!.message9_2,
-                  textAlign: TextAlign.start, style: textStyle2),
-              Text(networkName, textAlign: TextAlign.start, style: textStyle3),
-              Text(AppLocalizations.of(context)!.message9_3,
-                  textAlign: TextAlign.start, style: textStyle2),
-              Text(loomName, textAlign: TextAlign.start, style: textStyle3),
-              Text(AppLocalizations.of(context)!.message9_4,
-                  textAlign: TextAlign.start, style: textStyle2),
-            ]),
-          ),
-          LoomButton(
-            onPressed: () =>
-                context.read<LoomBloc>().add(LoomOpenButtonsEvent()),
-            text: AppLocalizations.of(context)!.next,
-            loomEvent: LoomOpenSuccessfulEvent(),
-          ),
-        ],
+            LoomButton(
+              onPressed: () =>
+                  context.read<LoomBloc>().add(LoomOpenButtonsEvent()),
+              text: AppLocalizations.of(context)!.perfectly,
+              loomEvent: LoomOpenSuccessfulEvent(),
+            ),
+            SizedBox(height: 20.0 * devicePixelRatio),
+          ],
+        ),
       ),
     );
   }
