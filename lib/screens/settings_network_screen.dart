b59987c45e012efa0838459fd3b9e0dfaa28e17f
AlteredGame <ralegond@gmail.com>
2021-10-29 13:31:05 +0300

    http logger + svg

--- lib/screens/settings_network_screen.dart
+++ lib/screens/settings_network_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loom/bloc/loom/loom_bloc.dart';
 import 'package:loom/widget/loom_app_bar.dart';
 import 'package:loom/widget/loom_body.dart';
